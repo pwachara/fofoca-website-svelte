e9cc560a5a9a4cc1809b9c85e7b7212e128deedb
Philemon Wachara <pwachara@hotmail.com>
2023-10-26 21:59:13 +0300

    initial commit after merging

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+	import '../app.postcss';
+	import { AppShell, AppBar } from '@skeletonlabs/skeleton';
+</script>
+
+<!-- App Shell -->
+<AppShell>
+	<svelte:fragment slot="header">
+		<!-- App Bar -->
+		<AppBar>
+			<svelte:fragment slot="lead">
+				<strong class="text-xl uppercase">Skeleton</strong>
+			</svelte:fragment>
+			<svelte:fragment slot="trail">
+				<a
+					class="btn btn-sm variant-ghost-surface"
+					href="https://mail.zoho.com"
+					target="_blank"
+					rel="noreferrer"
+				>
+					Staff Email
+				</a>
+			</svelte:fragment>
+		</AppBar>
+	</svelte:fragment>
+	<!-- Page Route Content -->
+	<slot />
+</AppShell>
+
+<style>
+	* {
+		scroll-behavior: smooth;
+	}
+</style>
