a392298384e72c6aa301800dea8950a2e230b1c9
Philemon Wachara <pwachara@hotmail.com>
2023-11-03 06:41:45 +0300

    replaced divider in about page with span element

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -57,13 +57,14 @@
                 <h3 class="font-semibold text-sm text-gray-500 text-center">
                     TRUSTED BY TEAMS FROM AROUND THE WORLD
                 </h3>
-                <div class="mt-6">
-                    <ul class="flex gap-y-6 flex-wrap items-center justify-center [&>*]:px-12 lg:divide-x">
+                <div class="mt-6 flex">
+                    <ul class="flex gap-y-6 flex-wrap items-center justify-center">
                         <li class="flex-none">
                             <img src="https://uploads-ssl.webflow.com/623a29a434fe9111ecdccb63/623a2d6bd1008ebeeabbee14_Group%2029.png" 
                                 class="w-48 h-24" 
                                 alt="Africa AI logo" />
                         </li>
+                        <li><span class="pr-12 border-r-4 border-slate-500 mr-12 text-5xl"></span></li>
                         <li class="flex-none">
                             <img src="https://www.apni.net/wp-content/uploads/2020/07/APNI-header-82.png"
                                  class="w-48 h-12"
