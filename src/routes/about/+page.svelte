144ae6c89523ff7aee0e1106f3061933c8c0816b
Philemon Wachara <pwachara@hotmail.com>
2023-11-12 09:54:51 +0300

    reduced values image size

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -22,42 +22,47 @@
 	<svelte:fragment slot="panel">
         <div class="text-gray-400 min-h-screen">
             {#if tabSet === 0}
-                <section id="about-us" class="flex mx-auto p-10 lg:mx-[15%] min-h-screen">
-                    <div class="flex flex-col overflow-hidden rounded-md shadow-sm lg:flex-row">
-                        <img src="/at-work-touching-helmet.jpg" alt="" class="h-3/4 dark:bg-gray-500 aspect-auto" loading="lazy">
-                        <div class="flex flex-col justify-center flex-1 p-4 dark:bg-gray-900 -mt-24">
-                            <h3 class="flex text-3xl font-bold mt-4 lg:mt-0 mx-auto">Our history</h3>
-                            <div class="my-6 lg:p-12 dark:text-gray-400 text-xl">
-                                <p class="p-6 flex flex-row">
-                                    <span class="p-2 text-[#fbbf24]">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-                                            <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
-                                        </svg>
-                                    </span>
-                                    <span>
-                                        Loka Advisory Services Limited is a HR Advisory and Consultancy services company that was formed in Kenya to ease the burden and step in, when no HR expertise exists to handle your continually needed HR functions.  
-                                    </span>
-                                </p>
-                                <p class="p-6 flex flex-row">
-                                    <span class="p-2 text-[#fbbf24]">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-                                            <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
-                                        </svg>
-                                    </span>
-                                    <span>
-                                        Our functions are typically split between a variety of people, who do not have the training and experience that a HR Consultant brings to the table.
-                                    </span>
-                                </p>
-                                <p class="p-6 flex flex-row">
-                                    <span class="p-2 text-[#fbbf24]">
-                                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
-                                            <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
-                                        </svg>
-                                    </span>
-                                    <span>
-                                        Loka Advisory lessens the burden for the employer by helping them maximize efficiency of employees leading to higher productivity levels.  Your business will benefit from the expertise and experience of Human Resource Consultants without the cost of paying a full-time salary.
-                                    </span>
-                                </p>
+                <section id="about-us" class="flex mx-auto p-10 xl:mx-[15%] min-h-screen">
+                    <div class="flex overflow-hidden rounded-md shadow-sm">
+                        <div class="flex flex-col">
+                            <div class="">
+                                <img src="/at-work-touching-helmet.jpg" alt="at-work" loading="lazy" class="h-64 w-64" />
+                            </div>                        
+                            <!-- <img src="/at-work-touching-helmet.jpg" alt="" class="md:h-2/3 dark:bg-gray-500 object-cover" loading="lazy"> -->
+                            <div class="flex flex-col justify-center p-4 dark:bg-gray-900 -mt-36">
+                                <h3 class="flex text-3xl font-bold mt-4 md:mt-0 mx-auto">Our history</h3>
+                                <div class="lg:p-12 dark:text-gray-400 text-xl">
+                                    <p class="p-6 flex flex-row">
+                                        <span class="p-2 text-[#fbbf24]">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                                                <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
+                                            </svg>
+                                        </span>
+                                        <span>
+                                            Loka Advisory Services Limited is a HR Advisory and Consultancy services company that was formed in Kenya to ease the burden and step in, when no HR expertise exists to handle your continually needed HR functions.  
+                                        </span>
+                                    </p>
+                                    <p class="p-6 flex flex-row">
+                                        <span class="p-2 text-[#fbbf24]">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                                                <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
+                                            </svg>
+                                        </span>
+                                        <span>
+                                            Our functions are typically split between a variety of people, who do not have the training and experience that a HR Consultant brings to the table.
+                                        </span>
+                                    </p>
+                                    <p class="p-6 flex flex-row">
+                                        <span class="p-2 text-[#fbbf24]">
+                                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                                                <path fill-rule="evenodd" d="M10.788 3.21c.448-1.077 1.976-1.077 2.424 0l2.082 5.007 5.404.433c1.164.093 1.636 1.545.749 2.305l-4.117 3.527 1.257 5.273c.271 1.136-.964 2.033-1.96 1.425L12 18.354 7.373 21.18c-.996.608-2.231-.29-1.96-1.425l1.257-5.273-4.117-3.527c-.887-.76-.415-2.212.749-2.305l5.404-.433 2.082-5.006z" clip-rule="evenodd" />
+                                            </svg>
+                                        </span>
+                                        <span>
+                                            Loka Advisory lessens the burden for the employer by helping them maximize efficiency of employees leading to higher productivity levels.  Your business will benefit from the expertise and experience of Human Resource Consultants without the cost of paying a full-time salary.
+                                        </span>
+                                    </p>
+                                </div>
                             </div>
                         </div>
                     </div>
